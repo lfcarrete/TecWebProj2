07b07a58e18637083595a5c890ef09a5449ec52c
Manuel Castanares <manuel.castanares14@gmail.com>
2020-11-05 09:17:19 -0300

    a

--- proj2-front/src/country.jsx
+++ proj2-front/src/country.jsx
@@ -2,19 +2,76 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class Country extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = { listaCountries : [
+            {country: "England"},
+            {country: "Brazil"}
+        ] };
+        
+        console.log("Teste")
+
+    
+
+    }
+
+    componentDidMount() {
+        const fetchData = async () => {
+            const response = await axios.get('http://localhost:3003/country/searchCountry/Italy')
+            .catch(erro => console.log(erro))
+            
+            if (!response.data) {
+                return 
+            }
+
+            var handleChange = (prevState, r) => {
+                prevState.listaCountries = r;
+                console.log(prevState)
+                return prevState;
+            }
+            
+            this.setState(handleChange(this.state, response.data.provinces), () => console.log(this.state))
+
+            
+        }
+        fetchData() 
+    }
+
     render() {
-        var countries = [
-            {country: 'England'},
-            {country: 'Brazil'}
-        ]
-        var liCountries = countries.map(pais => {
+
+        var countries = this.state.listaCountries;
+        console.log(this.state)
+
+        var liCountries = countries.map(function(pais) {
             return (
-            <li key={pais.country}>{pais.country}</li>
+                <dl>
+                    <dt>
+                        Country: {pais.province}
+                    </dt>
+                    <dd>
+                        Confirmed: {pais.confirmed}
+                    </dd>
+                    <dd>
+                        Recovered: {pais.recovered}
+                    </dd>
+                    <dd>
+                        Deaths: {pais.deaths}
+                    </dd>
+                    <dd>
+                        Active: {pais.active}
+                    </dd>
+                </dl>
+                
+
             )
         })
         return (
             <div>
-                <ul> {liCountries} </ul>
+                <dl>
+                    {liCountries}
+                </dl>
             </div>
         )
     }
