2ee1278009f22f70baa85130afa9e8cde8f7759c
Manuel Castanares <manuel.castanares14@gmail.com>
2020-11-06 17:25:07 -0300

    .

--- proj2-front/src/Login.jsx
+++ proj2-front/src/Login.jsx
@@ -1,17 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component, useImperativeHandle } from 'react'
+import axios from 'axios'
 
 export default class Login extends Component {
 
+    constructor(props) {
+        super(props)
+        this.onSubmit = this.onSubmit.bind(this)
+        this.state = { listaUsuarios : [
+            {country: "England"},
+            {country: "Brazil"}
+        ] };
+        
+        console.log("Teste")
+
+        axios.get('http://localhost:3003/users/')
+            .then(resp => {
+                this.setState({listaUsuarios: resp.data})
+                return;
+            })
+            .catch(erro => console.log(erro))
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        var user = this.user;
+        var password = this.password;
+        console.log(user)
+    }
+
     render() {
+        var usuarios = this.state.listaUsuarios
+        console.log(usuarios)
+        
+
+
         return (
-            <div>
-                <form method="get">
+            <div>  
+                <h1>Login</h1>
+                <form>
                     <label for="user">Username:</label><br />
-                    <input type="text" id="user" name="user"></input><br />
+                    <input type="text" id="user" ref={(c) => this.user = c} name="user"></input><br />
                     <label for="password">Password:</label><br />
-                    <input type="password" id="password" name="password"></input><br />
-                    <input type="submit" value="Submit"></input>
+                    <input type="password" id="password" ref={(c) => this.password = c}name="password"></input><br />
                 </form>
+                <button type="button" onClick={this.onSubmit}>Login</button>
             </div>
         );
     }
--- proj2-front/src/routes.jsx
+++ proj2-front/src/routes.jsx
@@ -4,10 +4,19 @@ import AllCountries from './AllCountries'
 
 import Country from './country'
 
+import Login from './Login'
+
+import Usuario from './Usuario'
+
+import Cadastrar from './Cadastrar'
+
 export default props => (
     <Router>
         <Route path='/AllCountries' component={AllCountries} />
         <Route path='/country' component={Country} />
-        <Redirect from='*' to='/AllCountries' />
+        <Route path='/Login' component={Login} />
+        <Route path='/Usuario' component={Usuario} />
+        <Route path='/Cadastrar' component={Cadastrar} />
+        <Redirect from='*' to='/Usuario' />
     </Router>
 )
